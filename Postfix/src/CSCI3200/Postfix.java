6e5838c1d13d50522a5e1c8f0f8a40bbc5311c6b
Mojog12 <mojog1212@gmail.com>
2026-04-09 16:53:34 -0400

    Add files via upload

---
+++ Postfix/src/CSCI3200/Postfix.java
@@ -0,0 +1,28 @@
+//--------------------------------------------------------------
+//Group Members: Michael Ojog, Denise Tepei
+//Date: April 25, 2026
+//Course: Data Structures and Algorithm Analysis (CSCI 3200)
+//Project Description: Postfix Calculator using Stacks
+//--------------------------------------------------------------
+
+package CSCI3200;
+import java.util.Stack;
+
+public class Postfix {
+    public static void main(String[] args) {
+        //create a stack using the Stack class available in the Java API of type Integer
+        Stack<Integer> stack = new Stack<>();
+
+        //create a PostfixCalculator object.
+        PostfixCalculator calculator = new PostfixCalculator();//it needs a string parameter but the instructions don't specify a string in main class??
+
+        //Open, read and evaluate the postfix expressions represented on each line
+        //in the file. Separate each token in the expression using a space.
+        //For example, 12 1 +
+
+
+        //Write the results of the evaluation to a file by popping and writing the single value in the stack.
+
+
+    }
+}
---
+++ Postfix/src/CSCI3200/PostfixCalculator.java
@@ -0,0 +1,55 @@
+package CSCI3200;
+
+import java.util.Stack;
+
+public class PostfixCalculator {
+    //Declare a String reference variable that contains the postfix expression
+    //Note: add encapsulation.
+    private String postfixExpression;
+    //Define a default constructor. Parameter: none.
+    public PostfixCalculator(){
+        
+    }
+    //Define a method named evaluatePostfix that evaluates the postfix expression
+    //entered by the user. Parameter:Stack<Integer>. Return type: boolean
+    public void evaluatePostfix(Stack<Integer> stack){
+        
+    }
+
+    // while there is more of the expression to read{
+
+    // if (the next input is an integer)
+
+    // Read the next integer and push it onto the stack.
+
+    // else-if(the next input is an operator)
+
+    // {
+
+    //Read the next character, which is an operator.
+
+    // Pop two numbers off the stack.
+
+    // Combine the two numbers with the operator using the
+    
+    // second number popped as the left operand) and push
+    // the result onto the stack.)
+
+    //}
+
+    //else
+
+        // This is an invalid expression
+
+    //}
+    
+    // At this point, the stack contains one number, which is the result of
+    // the expression. Otherwise, it is an invalid expression.
+
+    //Define a toString method that displays the postfix expression stored in the data field.
+    // Parameter: non. return type: String.
+
+    public String toString(){
+
+    }
+}
